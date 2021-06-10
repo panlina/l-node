d075c9e36f752a7acf017aac232fbe8776417a75
Jack Lu <jacklu@jacklu.me>
2021-06-10 23:56:50 +0800

    catch errors

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var compile = require('l/compile');
 var interpretation = require('l/test/f');
 
 var [, , source] = process.argv;
-var s = parse(source);
-var f = compile(s, new Environment(new Scope({ console: 'variable', fs: 'variable' })), interpretation);
-var environment = new Environment(new Scope({ console: require('./environment/console'), fs: require('./environment/fs') }));
-var v = f(environment);
-console.log(v);
+try {
+	var s = parse(source);
+	var f = compile(s, new Environment(new Scope({ console: 'variable', fs: 'variable' })), interpretation);
+	var environment = new Environment(new Scope({ console: require('./environment/console'), fs: require('./environment/fs') }));
+	var v = f(environment);
+	console.log(v);
+} catch (e) {
+	console.error(e.message);
+}
