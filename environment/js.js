399bf508606b1e0f0276870aa4f4d233faa07a6a
Jack Lu <jacklu@jacklu.me>
2021-10-13 14:04:59 +0800

    js: +callback

--- environment/js.js
+++ environment/js.js
@@ -1,3 +1,4 @@
 module.exports = {
-	call: ([f, _this, arguments]) => f.apply(_this, arguments)
+	call: ([f, _this, arguments]) => f.apply(_this, arguments),
+	callback: f => { return function () { return f([this, arguments]); } }
 };
